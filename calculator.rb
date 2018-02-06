18ee3234922d0b9fbc779948b80d13b27fe653e5
ildarmgt <ildarmgt@gmail.com>
2018-02-06 16:42:37 -0500

    small fix

--- calculator.rb
+++ calculator.rb
@@ -16,9 +16,12 @@ class Calculator
   end
 
   def divide(value1, value2)
-    return value1 / value2
+    return value1.to_f / value2.to_f
   end
 
 end
 
 puts "3 + 5 = " + Calculator.new.add(3, 5).to_s
+puts "3 - 5 = " + Calculator.new.subtract(3, 5).to_s
+puts "3 * 5 = " + Calculator.new.multiply(3, 5).to_s
+puts "3 / 5 = " + Calculator.new.divide(3, 5).to_s
